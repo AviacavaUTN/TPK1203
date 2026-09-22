fca891ce9d69687835580fa17d94daa4ce042bf8
AviacavaUTN <aviacava@frba.utn.edu.ar>
2026-09-21 21:46:27 -0300

    ventas actual

--- ventas.cpp
+++ ventas.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 #include <cstdio>
 using namespace std;
-/*Pide la fecha del día una sola vez al empezar y abre (o crea) la 
-planilla de ese día. Después, por cada venta: pide el mozo y su 
-clave y los valida, busca el precio, calcula la comisión y agrega la 
-venta a la planilla del día. Al terminar, la deja ordenada por mozo.
-
-mozos.dat, inventario.dat → comandas_dd-mm-aaaa.dat (crea/agrega), inventario.dat*/
 
 struct ComandaHistorica {
 	char  fecha[11];         // "DD-MM-AAAA" 
@@ -39,11 +33,13 @@ const float TASA_COMISION = 0.10f;   // la comision de cada venta es el 10% de l
 
 float precio(const char* Archivo, int cod); // busca el pecio
 void actualizar(const char* Archivo, int cod, int cantidadVendida); // actualiza el stock
-void planilla(int dia, int mes, int anio); // crea la planilla del día
 void pedirventa(const char* Archivo); // pide la venta
 bool valido(int cod,const char* Archivo);// verifica el mozo
+bool password(char pass[], char mpass[]); // verifica la contraseña del mozo
 void ordenar(const char* Archivo); // ordena la planilla por mozo
-bool password(const char* Archivo, int cod); // verifica la contraseña del mozo
+void planilla(int dia, int mes, int anio, char nombre[]); // crea la planilla del día
+void ordenamientoBurbuja(Comanda vec[], int len); // ordena el vector de comandas por mozo
+bool validarFecha(int dia, int mes, int anio); // valida la fecha ingresada
 
 
 int main(){
@@ -51,25 +47,15 @@ int main(){
 	int dia, mes, anio; // Pide la fecha del día
 	cout << "Ingrese la fecha (DD MM AAAA): ";
 	cin >> dia >> mes >> anio;
-		while (dia < 1 || dia > 31 || mes < 1 || mes > 12 || anio < 2026 ) {    // valida que la fecha sea correcta
-			cout << "Fecha inválida. Ingrese la fecha nuevamente (DD MM AAAA): ";
-			cin >> dia >> mes >> anio;
-		}
-		
-		const char* archivo = planilla(dia, mes, anio); // crea la planilla del día
-	
-
-	// Abre o crea la planilla de ese día 
-		/*falta asignarle el nombre al archivo*/
-	char nombreArchivo[30];
-	sprintf(nombreArchivo, "comandas_%s.dat", fecha);// ver como se usa
-	FILE* archivoComandas = fopen(nombreArchivo, "ab+");
-	if (archivoComandas == NULL) {
-		cout << "Error al abrir el archivo de comandas." << endl;
-		return 1;
+	bool fecha = validarFecha(dia, mes, anio);
+	while (!fecha) {
+		cout << "fecha incorrecta, ingresela nuevamente" << endl;
+		cin >> dia >> mes >> anio; 
+		fecha = validarFecha(dia, mes, anio);
 	}
-
-
+	char planillaDia[30];
+	planilla(dia, mes, anio, planillaDia); // crea la planilla del día
+	pedirventa(planillaDia	); 
 
 	return 0;
 }
@@ -119,7 +105,7 @@ void actualizar(const char* Archivo, int cod, int cantidadVendida) {
 				fwrite(&prod, sizeof(Producto), 1, f); // escribo el producto actualizado
 				cout << "Stock actual: " << prod.stockActual << endl; // muestro el stock actual
 				if (prod.stockActual <= 0) {
-					cout << "Ya no hay stock suficiente" << endl;// si no hay mas stock
+					cout << "stock agotado" << endl;// si no hay mas stock
 				}
 				fclose(f);// cierro el archivo 
 				cout << "Stock actualizado con éxito" << endl;
@@ -146,7 +132,7 @@ void pedirventa(const char* Archivo) {
 		return;
 	}
 	int n = 1;
-	comanda c;
+	Comanda c;
 	while (n != 0) {
 
 		cout << "Ingrese el código de mozo: ";
@@ -156,11 +142,16 @@ void pedirventa(const char* Archivo) {
 			cout << "Código de mozo inválido" << endl;
 			cout << "ingrese un codigo de mozo valido" << endl;
 			cin >> c.idMozo;
+			esValido = valido(c.idMozo, "mozos.dat");
 		}
 		cout << "Ingrese código del producto: ";
 		cin >> c.codigoProducto;
 		cout << "Ingrese cantidad vendida: ";
 		cin >> c.cantidad;
+		while (c.cantidad <= 0) {
+			cout << "ingesaste una cantidad incorrectar, vuelve a ingresarla " << endl;
+			cin >> c.cantidad;
+		}
 		float p = precio("inventario.dat", c.codigoProducto);
 		while (p == -1) {
 			cout << "Producto inexistente" << endl;
@@ -169,15 +160,16 @@ void pedirventa(const char* Archivo) {
 			p = precio("inventario.dat", c.codigoProducto);
 		}
 		c.comision = p * c.cantidad * TASA_COMISION;
-		fwrite(&c, sizeof(comanda), 1, f);
+		fwrite(&c, sizeof(Comanda), 1, f);
 		actualizar("inventario.dat", c.codigoProducto, c.cantidad);
 		cout << "Ingrese 0 para terminar o 1 para continuar: ";
 		cin >> n;
 	
 	}	
+	fclose(f); // cierro el archivo para poder usar ordenar 
 	ordenar(Archivo);
-	fclose(f);
 }
+
 bool valido(int cod,const char* Archivo) {
 	FILE* f = fopen(Archivo, "rb");
 	if (f == NULL) {
@@ -188,9 +180,10 @@ bool valido(int cod,const char* Archivo) {
 	char pass[20];
 	int leido = fread(&m, sizeof(Mozo), 1, f);
 	while (leido == 1) {
-		if (cod == m.codigo) {
+		if (cod == m.idMozo) {
 			cout << "Ingrese password: ";
 			cin >> pass;
+			fclose(f);
 			return password(pass, m.password);
 		}
 		leido = fread(&m, sizeof(Mozo), 1, f);
@@ -199,13 +192,110 @@ bool valido(int cod,const char* Archivo) {
 	cout << "Mozo no encontrado." << endl;
 	return false;
 }
+
 bool password(char pass [], char mpass[]) {
-	if (pass == mpass) {
-		cout << "Mozo encontrado y password correcto." << endl;
-		return true;
+	int k = 8;
+	int i = 0;
+	while (pass[i] != '\0' && mpass[i] != '\0') {
+		if (pass[i] + k != mpass[i]) {
+			cout << "contraseña incorrecta" << endl;
+			return false;
+		}
+		i++;
 	}
-	else {
-		cout << "Password incorrecto." << endl;
+	if (pass[i] != '\0' || mpass[i] != '\0'){
+		cout << "contraseña incorrecta" << endl;
 		return false;
 	}
+	return true;
+}
+
+void ordenar(const char* Archivo) { // funcion que pasa el archivo a un array y lo ordena por mozo
+	FILE* f = fopen(Archivo, "rb");
+	if (f == NULL) {
+		cout << "Error al abrir el archivo." << endl;
+		return;
+	}
+	Comanda vec[900];
+	int len = 0;
+	Comanda c;
+	int leido = fread(&c, sizeof(Comanda), 1, f);
+	while (leido == 1) { // paso el archivo a un array 
+		if (len < 900) {
+			vec[len] = c;
+			len++;
+		}
+		if (len >= 900) {
+			cout << "error" << endl;
+			return;
+		}
+		leido = fread(&c, sizeof(Comanda), 1, f);
+	}
+
+	fclose(f);
+	ordenamientoBurbuja(vec, len); // ordeno el array por mozo
+	FILE* f2 = fopen(Archivo, "wb");
+	if (f2 == NULL) {
+		cout << "Error al abrir el archivo." << endl;
+		return;
+	}
+	for (int i = 0; i < len; i++) {
+		fwrite(&vec[i], sizeof(Comanda), 1, f2); // paso el array a un archivo otra vez
+	}
+	
+	fclose(f2);
+}
+
+void planilla(int dia, int mes, int anio, char nombre[]) {
+// Abre o crea la planilla de ese día 
+   sprintf(nombre, "comandas_%d-%d-%d.dat", dia,mes,anio);
+	FILE* f = fopen(nombre, "ab+");
+	if (f == NULL) {
+		cout << "Error al abrir el archivo de comandas." << endl;
+		return;
+	}
+	fclose(f);
+}
+
+void ordenamientoBurbuja(Comanda vec[], int len) {
+	for (int i = 0; i < len - 1; i++) {
+		for (int j = 0; j < len - i - 1; j++) {
+			if (vec[j].idMozo > vec[j + 1].idMozo) {
+				Comanda temp = vec[j];
+				vec[j]= vec[j + 1];
+				vec[j + 1] = temp;
+			}
+		}
+	}
+}
+bool validarFecha(int dia, int mes, int anio) {
+	if (dia < 1 || dia > 31) {
+		return false;
+	}
+	if (mes < 1 || mes > 12) {
+		return false;
+	}
+	if (anio < 2026) {
+		return false;
+	}
+	switch (mes) {
+	case 1:
+	case 3: 
+	case 5: 
+	case 7:
+	case 8:
+	case 10:
+	case 12:
+		return dia <= 31;
+
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		return dia <= 30;
+
+	case 2:
+		return dia <= 29;
+	}
+	return false;
 }
